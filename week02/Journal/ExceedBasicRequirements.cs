df4c00b23b3bd4f7a7a6d14c01ee2790ed04a04a
LuisE <escarateevoraluis@gmail.com>
2025-03-10 15:27:46 -0600

    Exceed basic requirements comments

---
+++ week02/Journal/ExceedBasicRequirements.cs
@@ -0,0 +1,12 @@
+// Added improvements to go beyond the basic requirements:
+// 1. Saving and loading in JSON:
+//    I decided to use JSON format for saving and loading the journal data. This format is pretty common and easy to work with. 
+//    It also makes the data look more organized and can be used in other programs if needed.
+// 2. Journal statistics:
+//    I added a menu option that shows some statistics about the journal, like:
+//       - How many entries the journal has.
+//       - The date of the first entry.
+//       - The date of the most recent entry.
+//    I think this is helpful for users to see how they are doing with their journaling habit.
+// 3. More prompts:
+//    I added more prompts to make it more interesting. Now there are more options to choose from, which can help if someone doesn’t know what to write about.
