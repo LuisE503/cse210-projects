ef240dd1b0e206aedfdbef54a8c7b9ce6430b680
LuisE <escarateevoraluis@gmail.com>
2025-03-18 10:50:45 -0600

    Changes_4 edit creativity requeriments

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,8 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        Scripture scripture = new Scripture(reference, "Trust in the Lord with all your heart and lean not on your own understanding");
+        // Creamos la biblioteca de escrituras
+        ScriptureLibrary library = new ScriptureLibrary();
+        library.AddScripture(new Scripture(new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all your heart and lean not on your own understanding"));
+        library.AddScripture(new Scripture(new Reference("John", 3, 16), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life"));
+        library.AddScripture(new Scripture(new Reference("Psalm", 23, 1), "The Lord is my shepherd; I shall not want"));
+
+        // Elegimos una escritura al azar
+        Scripture scripture = library.GetRandomScripture();
 
         while (true)
         {
