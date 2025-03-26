44e23232c75146aa2bb3493ef20e74afb07c49ba
LuisE <escarateevoraluis@gmail.com>
2025-03-26 08:45:29 -0600

    week_5_more requeriments

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        int breathingCount = 0;
+        int reflectingCount = 0;
+        int listingCount = 0;
+
         Console.WriteLine("Mindfulness Program");
         Console.WriteLine("1. Breathing Activity");
         Console.WriteLine("2. Reflection Activity");
@@ -22,17 +26,25 @@ class Program
                 case "1":
                     BreathingActivity breathing = new BreathingActivity();
                     breathing.RunActivity();
+                    breathingCount++; 
                     break;
                 case "2":
                     ReflectionActivity reflecting = new ReflectionActivity();
                     reflecting.RunActivity();
+                    reflectingCount++; 
                     break;
                 case "3":
                     ListingActivity listing = new ListingActivity();
                     listing.RunActivity();
+                    listingCount++; 
                     break;
                 case "4":
                     running = false;
+
+                    Console.WriteLine("\nActivity Summary:");
+                    Console.WriteLine($"Breathing Activity completed: {breathingCount} times.");
+                    Console.WriteLine($"Reflection Activity completed: {reflectingCount} times.");
+                    Console.WriteLine($"Listing Activity completed: {listingCount} times.");
                     Console.WriteLine("Goodbye!");
                     break;
                 default:
