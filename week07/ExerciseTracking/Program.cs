ac434a3c94182ba5d43eb3d22a446664dc44ff50
LuisE <escarateevoraluis@gmail.com>
2025-03-28 13:44:36 -0600

    week07_change2

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        var running = new Running("03 Nov 2022", 30, 5.0);      // Distancia en km
+        var cycling = new Cycling("03 Nov 2022", 40, 15.0);     // Velocidad en kph
+        var swimming = new Swimming("03 Nov 2022", 25, 20);     // Laps en la piscina
+
+        List<Activity> activities = new List<Activity> { running, cycling, swimming };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
